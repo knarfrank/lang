4087006ccf8b462dbd1f98645071b9c81a2ba1fa
frank_hemsworth <fh13286@my.bristol.ac.uk>
2015-02-20 22:24:06 +0000

    basic tokeniser implemented

--- main.go
+++ main.go
@@ -1,8 +1,61 @@
 package main
 import (
   "fmt"
+  "regexp"
 )
 
+
+
+
 func main() {
-  fmt.Println(MOD)
+  program := "43 ^ 34 & 34 | ~345"
+
+  var lastToken token
+  currentPos := 0
+
+  program = program + " "
+  for i:=1;i<=len(program);i++ {
+    if currentToken := getToken(string(program[currentPos:i])); currentToken.token == IGNR {
+      if lastToken.token != IGNR {
+        fmt.Print(lastToken.token)
+        fmt.Println(": " + lastToken.value)
+        i--
+      }
+      currentPos = i
+      lastToken = currentToken
+    } else {
+      //fmt.Println(" != "+currentToken.value)
+      lastToken = currentToken
+    }
+
+  }
+
+}
+
+
+func getToken(s string) token {
+  tokens := getTokens()
+  for _, t := range tokens {
+    // We return the first one that matches.
+    if(checkToken(t, s)) {
+      t.value = s
+      return t
+    }
+  }
+  return token{IGNR, "", ""}
+}
+
+func checkToken(t token, substr string) bool {
+  match, err := regexp.MatchString(t.regex, substr)
+  checkError(err);
+  if match {
+    return true
+  }
+  return false
+}
+
+func checkError(err error) {
+  if err != nil {
+    fmt.Println(err)
+  }
 }
--- tokens.go
+++ tokens.go
@@ -2,12 +2,18 @@ package main
 
 
 const (
-  IGRE = iota
+  IGNR = iota
   /*
-        Types
+        Identifier
+  */
+  IDFR
+
+  /*
+        Constant Types
   */
   INT     // 343
   BOOL    // true/false
+  STRG    // "sdf"
 
   /*
         Arthimetic Operators
@@ -24,8 +30,52 @@ const (
   AND     // &
   OR      // |
   XOR     // ^
-  NOT     // ~
+  BNOT    // ~
   RSHFT   // >>
   LSHFT   // <<
 
+
+  /*
+      Boolean Logical Operators
+  */
+  EQU    // ==
+  LST    // <
+  GRT    // >
+  LNOT   // !
+
+  /*
+      Keywords
+  */
+  IF     // "if"
+  ELSE   // "else"
+  VAR    // "var"
+
+
 )
+
+type token struct {
+  token int
+  value string
+  regex string
+}
+
+
+func getTokens() []token {
+  var tokens []token
+  tokens = make([]token, 0, 0)
+  tokens = append(tokens, token{IDFR, "", "^[a-z][a-z]*$"},
+                          token{INT, "", "^[0-9]+$"},
+                          token{BOOL, "", "^(true|false)$"},
+                          token{BOOL, "", "^(true|false)$"},
+                          token{ADD, "", "^\\+$"},
+                          token{SUB, "", "^-$"},
+                          token{MUL, "", "^\\*$"},
+                          token{DIV, "", "^/$"},
+                          token{MOD, "", "^%$"},
+                          token{AND, "", "^&$"},
+                          token{OR, "", "^\\|$"},
+                          token{XOR, "", "^\\^$"},
+                          token{BNOT, "", "^~$"})
+
+  return tokens;
+}
