fec3987501bc6fbfe01493dade8b5f58115e8e76
frank_hemsworth <fh13286@my.bristol.ac.uk>
2015-02-20 18:29:49 +0000

    Started defining tokens for language.

---
+++ tokens.go
@@ -0,0 +1,31 @@
+package main
+
+
+const (
+  IGRE = iota
+  /*
+        Types
+  */
+  INT     // 343
+  BOOL    // true/false
+
+  /*
+        Arthimetic Operators
+  */
+  ADD     // +
+  SUB     // -
+  MUL     // *
+  DIV     // /
+  MOD     // %
+
+  /*
+        Bitwise Operators
+  */
+  AND     // &
+  OR      // |
+  XOR     // ^
+  NOT     // ~
+  RSHFT   // >>
+  LSHFT   // <<
+
+)
