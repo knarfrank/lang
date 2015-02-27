7376f49cbe2cbadeee95616378ede162da9c6f0f
frank_hemsworth <fh13286@my.bristol.ac.uk>
2015-02-27 17:52:38 +0000

    Added functions to classes

--- parser/parser.go
+++ parser/parser.go
@@ -12,6 +12,8 @@ const (
   IFSTATEMENT
   CLASS
   FUNCTION
+  PARAMETERS
+  RETURNTYPE
   BOOLEXPRESSION
   ASSIGNMENT
   EXPRESSION
@@ -32,6 +34,8 @@ func getLabel(l int) string {
     case IFSTATEMENT: return "IFSTATEMENT"
     case CLASS: return "CLASS"
     case FUNCTION: return "FUNCTION"
+    case PARAMETERS: return "PARAMETERS"
+    case RETURNTYPE: return "RETURNTYPE"
     case BOOLEXPRESSION: return "BOOLEXPRESSION"
     case ASSIGNMENT: return "ASSIGNMENT"
     case EXPRESSION: return "EXPRESSION"
@@ -218,14 +222,57 @@ func method(list []tokens.Token) (bool, *Tree) {
   i := 3
   for {
     if list[i].Token == tokens.LPAREN {
+      // parse parameters
+      if b, t := methodParams(list[3:i]); b {
+        addChild(ast, t)
+      } else {
+        // ?
+      }
       break
     }
     i++;
+  }
+  // check arrow for return type (Maybe i should make a new token?)
+  if list[i+1].Token != tokens.SUB || list[i+2].Token != tokens.GRT {
+    return false, ast
+  }
+
+  // Check for return type brackets
+  if list[i+3].Token != tokens.RPAREN || list[i+5].Token != tokens.LPAREN {
+    return false, ast
+  }
+
+  // Check return type
+  if c, t := methodReturnType(list[i+3]); c {
+    addChild(ast, t)
+  } else {
+    return false, ast
+  }
+  if len(list[i+7:len(list)-1]) != 0 {
+    stmts := new(Tree)
+    statements(stmts, list[i+7:len(list)-1])
+    addChild(ast, stmts)
+  } else {
+    // empty function
   }
 
   return true, ast
 }
 
+func methodReturnType(t tokens.Token) (bool, *Tree) {
+  ast := new(Tree)
+  ast.label = RETURNTYPE
+  return true, ast
+}
+
+
+func methodParams(list []tokens.Token) (bool, *Tree) {
+  ast := new(Tree)
+  ast.label = PARAMETERS
+  //fmt.Println(list)
+
+  return true, ast
+}
 /*
 
 
